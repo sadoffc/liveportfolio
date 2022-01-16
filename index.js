e4b91b9ff3d85ac19c93c675527f97bf0ba38fbb
CS884335 <CS884335@wcupa.edu>
2022-01-16 12:04:22 -0500

    pushing from local

---
+++ index.js
@@ -0,0 +1,58 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    let x = event.clientX * scaleFactor;
+    let y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast () {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_zjmmfqt",
+        "template_b3t51ff",
+        event.target,
+        "user_ymfDmx7FB21a8bepGPFsh"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly at: connorjsadoff@email.com"
+        );
+      });
+  }
+
+  function toggleModal() {
+      if (isModalOpen) {
+          isModalOpen = false;
+          return document.body.classList.remove("modal--open");
+      }
+      isModalOpen = true;
+      document.body.classList += " modal--open";
+  }
